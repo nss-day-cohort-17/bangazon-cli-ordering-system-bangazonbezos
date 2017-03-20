d7cef4f0f94f3cf6e260e8c130ea7a424560fce5
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-20 11:53:47 -0500

    edit cart fnc written, first prompt works

--- test/editCart-test.js
+++ test/editCart-test.js
@@ -1,8 +1,23 @@
-const {assert : {isFunction}} =require('chai');
-const {editCart} = require('../lib/editCart.js');
-
-describe ('editCart',()=>{
-  it('should be a function',()=>{
-    isFunction(editCart)
+const {assert : {isFunction, isNumber, isString}} =require('chai');
+const {editCart, quantity, confirmEdit} = require('../lib/editCart.js');
+describe('editCart',()=>{
+  describe ('editCart',()=>{
+    it('should be a function',()=>{
+      isFunction(editCart)
+    });
+    it('should return a number', ()=>{
+      isNumber(editCart());
+    })
   });
-});
+  describe('quantity', ()=>{
+    it('should return a number', ()=>{
+      isNumber(quantity());
+    })
+  });
+  describe('confirmEdit', ()=>{
+    it('should return a string', ()=>{
+      isString(confirmEdit());
+    })
+
+  })
+})
